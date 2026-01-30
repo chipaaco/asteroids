1c58d148e4f437e02cd0babf026a08ce466b5a15
chipaaco <169686750+chipaaco@users.noreply.github.com>
2026-01-30 21:06:54 +0000

    Add Player class and draw it on screen
    - Create Player class inheriting from CircleShape
    - Add player constants for radius and line width
    - Instantiate player at screen center in main loop
    - Draw player as triangle polygon with configurable width

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock, dt = pygame.time.Clock(), 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         # print(f"{dt}")
