566e86b20d7dd1c6ecb7d95425d6f9b0e7542079
chipaaco <169686750+chipaaco@users.noreply.github.com>
2026-01-31 19:02:41 +0000

    Add collision detection between shots and asteroids

--- main.py
+++ main.py
@@ -46,6 +46,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(obj):
+                    log_event("asteroid_shot")
+                    obj.kill()
+                    shot.kill()
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
